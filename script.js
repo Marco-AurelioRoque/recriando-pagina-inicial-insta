680c0454f7f5521d2f37f3a92a4ef5f407daa043
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-04-11 21:17:01 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,8 @@
+function removebg() {
+
+     document.getElementById("perfil").src = "https://th.bing.com/th/id/OIP.qcyt2AttpvWjiXUu54n2VwHaHa?w=192&h=192&c=7&o=5&pid=1.7"
+}
+
+function perfilmarco() {
+    document.getElementById("perfil").src = "https://avatars.githubusercontent.com/u/80369075?v=4"
+}
